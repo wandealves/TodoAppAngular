3566aaa72ab6295ace6f0939d3b0474fc62d54e6
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-03-01 10:42:37 -0300

    Foi criado os pages: all, new, today, tomorrow

--- Todo/src/app/app-routing.module.ts
+++ Todo/src/app/app-routing.module.ts
@@ -3,9 +3,20 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { TodayComponent } from './pages/today/today.component';
+import { TomorrowComponent } from './pages/tomorrow/tomorrow.component';
+import { AllComponent } from './pages/all/all.component';
+import { NewComponent } from './pages/new/new.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, },
+  {
+    path: '', component: HomeComponent, children: [
+      { path: '', component: TodayComponent },
+      { path: 'tomorrow', component: TomorrowComponent },
+      { path: 'all', component: AllComponent },
+      { path: 'new', component: NewComponent },
+    ]
+  },
   { path: 'login', component: LoginComponent, }
 ];
 
