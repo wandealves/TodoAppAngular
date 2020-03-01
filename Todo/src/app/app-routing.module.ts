e2f5bc4f87fcaa4e2614f49e8a0b2213b24d19fc
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-03-01 10:10:46 -0300

    Criação tela de login

--- Todo/src/app/app-routing.module.ts
+++ Todo/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [{ path: '', component: LoginComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
